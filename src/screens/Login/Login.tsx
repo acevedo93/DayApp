64a5be392e1d935c03c2d4101119830834058525
acevedo9303 <david.acevedo@vascarsolutions.com>
2022-04-08 17:14:15 -0500

    fix login ion ios

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,18 +1,27 @@
-import {Box, Text, Heading, Button, VStack} from 'native-base';
-
 import React from 'react';
+import {Box, Heading, Button, VStack} from 'native-base';
+
+import {login, logout} from '../../services/firebase/auth';
 
 export const LoginScreen = () => {
-  const handleLogin = (): void => {
-    console.log('Login action');
+  const handleLogin = () => {
+    login();
   };
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
     <Box background="info.10" flex="1" safeArea alignItems="center">
       <Box mt="50%" width="80%" rounded="lg" height="50%" background="info.25">
-        <VStack alignItems="center" space="10" padding="2">
+        <VStack alignItems="center" space="4" padding="10">
           <Heading color="info.200">Login</Heading>
           <Button onPress={handleLogin} bg="primary.100" color="info.200">
             Login with google
+          </Button>
+          <Button bg="primary.100" color="info.200" onPress={handleLogout}>
+            Logout
           </Button>
         </VStack>
       </Box>
--- src/services/firebase/auth/index.ts
+++ src/services/firebase/auth/index.ts
@@ -1,1 +1,20 @@
-export const hola = 'hola';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {auth} from '../config';
+
+GoogleSignin.configure({
+  webClientId:
+    '581406484901-9gkilhe6tpv2p63dd5ft6cnfkb5d5fsc.apps.googleusercontent.com',
+  offlineAccess: true,
+});
+
+export const login = async () => {
+  const {idToken} = await GoogleSignin.signIn();
+  console.log(idToken);
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  console.log(googleCredential);
+  return auth().signInWithCredential(googleCredential);
+};
+
+export const logout = async () => {
+  return await auth().signOut();
+};
