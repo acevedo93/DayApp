81b24a2978352b23963b0125b535aeb0656829d2
acevedo9303 <david.acevedo@vascarsolutions.com>
2022-04-11 15:52:36 -0500

    add terms and conditions

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import {Box, Heading, Button, VStack} from 'native-base';
-import {login} from '../../redux/slices/auth.slice';
+import React, {useState} from 'react';
+import {Box, Heading, Button, VStack, Text, Checkbox} from 'native-base';
 import {loginFirebase} from '../../services/firebase/auth';
 
 export const LoginScreen = () => {
+  const [terms, setTerms] = useState(false);
+
   const handleLogin = () => {
     loginFirebase();
   };
@@ -13,7 +14,22 @@ export const LoginScreen = () => {
       <Box mt="50%" width="80%" rounded="lg" height="50%" background="info.25">
         <VStack alignItems="center" space="4" padding="10">
           <Heading color="info.200">Login</Heading>
-          <Button onPress={handleLogin} bg="primary.100" color="info.200">
+          <Text>
+            Lorem ipsum vim dsadsads rewrwerew rewrew rew Lorem ipsum vim
+            dsadsads rewrwerew rewrew rewLorem ipsum vim dsadsads rewrwerew
+          </Text>
+          <Checkbox
+            colorScheme="success"
+            value="term"
+            onChange={val => setTerms(val)}
+            accessibilityLabel="Accpet terms and conditions">
+            Accept terms and conditions
+          </Checkbox>
+          <Button
+            disabled={!terms}
+            onPress={handleLogin}
+            bg="primary.100"
+            color="info.200">
             Login with google
           </Button>
         </VStack>
