b38ad79caac6f344d752f1dd5d0415bb6bdbcf88
acevedo9303 <david.acevedo@vascarsolutions.com>
2022-04-22 10:49:14 -0500

    added hook to close day

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import {Box, Heading, Center, Pressable} from 'native-base';
 import {DayContainer} from '../../components/DayContainer';
+import {useFinishDay} from '../../hooks/useFinishDay';
 
 export const HomeScreen = () => {
+  const {isTimeToCloseDay} = useFinishDay();
   return (
     <Box background="info.10" flex="1" px="4">
-      <Pressable onPress={() => console.log('pressable')}>
-        <Center
-          mt="10%"
-          width="100%"
-          rounded="lg"
-          height="60"
-          background="info.25"
-          alignItems="center"
-          borderWidth="1"
-          borderColor="info.300"
-          borderStyle="dashed">
-          <Heading color="info.200">Planear Nuevo dia</Heading>
-        </Center>
+      {!isTimeToCloseDay && (
+        <Pressable onPress={() => console.log('pressable')}>
+          <Center
+            mt="10%"
+            width="100%"
+            rounded="lg"
+            height="60"
+            background="info.25"
+            alignItems="center"
+            borderWidth="1"
+            borderColor="info.300"
+            borderStyle="dashed">
+            <Heading color="info.200">Planear Nuevo dia</Heading>
+          </Center>
+        </Pressable>
+      )}
 
-        <DayContainer />
-      </Pressable>
+      <DayContainer />
     </Box>
   );
 };
